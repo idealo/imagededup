a14bd89503633d186971fe8561ed046d0e9d1dc1
Tanuj Jain <tanuj.jain.10@gmail.com>
2019-05-03 19:46:17 +0200

    DS-522: Added dhash function that accepts path of one image and returns a 64 bit difference hash of the image.

--- imagededup/hashing.py
+++ imagededup/hashing.py
@@ -1,8 +1,15 @@
-import os
-import numpy as np
-from pathlib import Path
-from PIL import Image
 import scipy.fftpack
+import numpy as np
+from PIL import Image
+
+"""
+TODO: Put Image loading, resizing, grayscale conversion and arrayfication in one function
+Make another function for hash generation given 64 by 64 hash matrix
+Convert functions to type annotation format
+Add function to calculate hamming distance between hashes of 2 images
+Add functions to calculate hashes given a directory
+Add wavelet hash?
+"""
 
 
 class Hashing:
@@ -16,6 +23,7 @@ class Hashing:
         return '{:0x}'.format(int_bool)
 
     def phash(self, path_image):
+        """Implementation reference: http://www.hackerfactor.com/blog/index.php?/archives/432-Looks-Like-It.html"""
         im = Image.open(path_image)
         im_res = im.resize((32, 32))
         im_gray = im_res.convert('L')
@@ -44,4 +52,21 @@ class Hashing:
             calculated_hash.append(self._bool_to_hex(i))
 
         return ''.join(calculated_hash)
+
+    def dhash(self, path_image):
+        """Implementation reference: http://www.hackerfactor.com/blog/index.php?/archives/529-Kind-of-Like-That.html"""
+        im = Image.open(path_image)
+        im_res = im.resize((9, 8))
+        im_gray = im_res.convert('L')
+        im_gray_arr = np.array(im_gray).astype('int')
+        hash_mat = im_gray_arr[:, :-1] > im_gray_arr[:, 1:]
+
+        calculated_hash = []
+        for i in np.array_split(np.ndarray.flatten(hash_mat), 16):
+            calculated_hash.append(self._bool_to_hex(i))
+
+        return ''.join(calculated_hash)
+
+
+
 
