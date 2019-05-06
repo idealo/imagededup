5f9f393286d10c9424ee7508713547d41642c9f9
Tanuj Jain <tanuj.jain.10@gmail.com>
2019-05-06 11:50:59 +0200

    DS-522: Refactored hashing code to include type annotations and enahnced  modularity of the code.

--- imagededup/hashing.py
+++ imagededup/hashing.py
@@ -1,11 +1,11 @@
 import scipy.fftpack
 import numpy as np
 from PIL import Image
+from pathlib import Path
 
 """
-TODO: Put Image loading, resizing, grayscale conversion and arrayfication in one function
+TODO:
 Make another function for hash generation given 64 by 64 hash matrix
-Convert functions to type annotation format
 Add function to calculate hamming distance between hashes of 2 images
 Add functions to calculate hashes given a directory
 Add wavelet hash?
@@ -17,55 +17,46 @@ class Hashing:
         pass
 
     @staticmethod
-    def _bool_to_hex(x):
+    def _bool_to_hex(x: np.array) -> str:
         str_bool = ''.join([str(int(i)) for i in x])
-        int_bool = int(str_bool, 2)
+        int_bool = int(str_bool, 2) # int base 2
         return '{:0x}'.format(int_bool)
 
-    def phash(self, path_image):
-        """Implementation reference: http://www.hackerfactor.com/blog/index.php?/archives/432-Looks-Like-It.html"""
+    @staticmethod
+    def image_preprocess(path_image: Path, resize_dims = (int, int)) -> np.array:
         im = Image.open(path_image)
-        im_res = im.resize((32, 32))
-        im_gray = im_res.convert('L')
+        im_res = im.resize(resize_dims)
+        im_gray = im_res.convert('L') # convert to grayscale (i.e., single channel)
         im_gray_arr = np.array(im_gray)
-        dct_coef = scipy.fftpack.dct(scipy.fftpack.dct(im_gray_arr, axis=0), axis=1)
-        dct_reduced_coef = dct_coef[:8, :8]
-        mean_coef_val = np.mean(np.ndarray.flatten(dct_reduced_coef)[1:])
-        hash_mat = dct_reduced_coef >= mean_coef_val
+        return im_gray_arr
 
+    def get_hash(self, hash_mat: np.array, n_blocks: int) -> str:
         calculated_hash = []
-        for i in np.array_split(np.ndarray.flatten(hash_mat), 16):
+        for i in np.array_split(np.ndarray.flatten(hash_mat), n_blocks):
             calculated_hash.append(self._bool_to_hex(i))
-
         return ''.join(calculated_hash)
 
-    def ahash(self, path_image):
-        im = Image.open(path_image)
-        im_res = im.resize((8, 8))
-        im_gray = im_res.convert('L')
-        im_gray_arr = np.array(im_gray)
+    def phash(self, path_image: Path) -> str:
+        """Implementation reference: http://www.hackerfactor.com/blog/index.php?/archives/432-Looks-Like-It.html"""
+        im_gray_arr = self.image_preprocess(path_image, (32, 32))
+        dct_coef = scipy.fftpack.dct(scipy.fftpack.dct(im_gray_arr, axis=0), axis=1)
+        dct_reduced_coef = dct_coef[:8, :8] # retain top left 8 by 8 dct coefficients
+        mean_coef_val = np.mean(np.ndarray.flatten(dct_reduced_coef)[1:]) # average of coefficients exclusing the DC
+                                                                          # term (0th term)
+        hash_mat = dct_reduced_coef >= mean_coef_val # All coefficients greater than mean of coefficients
+        return self.get_hash(hash_mat, 16) # 16 character output
+
+    def ahash(self, path_image: Path) -> str:
+        im_gray_arr = self.image_preprocess(path_image, (8, 8))
         avg_val = np.mean(im_gray_arr)
         hash_mat = im_gray_arr >= avg_val
+        return self.get_hash(hash_mat, 16) # 16 character output
 
-        calculated_hash = []
-        for i in np.array_split(np.ndarray.flatten(hash_mat), 16):
-            calculated_hash.append(self._bool_to_hex(i))
-
-        return ''.join(calculated_hash)
-
-    def dhash(self, path_image):
+    def dhash(self, path_image: Path) -> str:
         """Implementation reference: http://www.hackerfactor.com/blog/index.php?/archives/529-Kind-of-Like-That.html"""
-        im = Image.open(path_image)
-        im_res = im.resize((9, 8))
-        im_gray = im_res.convert('L')
-        im_gray_arr = np.array(im_gray).astype('int')
-        hash_mat = im_gray_arr[:, :-1] > im_gray_arr[:, 1:]
-
-        calculated_hash = []
-        for i in np.array_split(np.ndarray.flatten(hash_mat), 16):
-            calculated_hash.append(self._bool_to_hex(i))
-
-        return ''.join(calculated_hash)
+        im_gray_arr = self.image_preprocess(path_image, (9, 8))
+        hash_mat = im_gray_arr[:, :-1] > im_gray_arr[:, 1:] # Calculates difference between consecutive columns
+        return self.get_hash(hash_mat, 16) # 16 character output
 
 
 
