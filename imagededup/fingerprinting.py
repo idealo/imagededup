1ec578b116d7a970cfc9cfe89e5e35419069c11f
valiantone <zrjohn@yahoo.com>
2019-05-17 15:53:49 +0200

    DS-529: Finalized notebook with working first example

--- imagededup/fingerprinting.py
+++ imagededup/fingerprinting.py
@@ -12,26 +12,39 @@ class Dataset:
     Object contains hashed image fingerprints as well, however, hashing method is set by user.
     """
     def __init__(self, path_to_queries: str, path_to_test: str) -> None:
-        self.query_docs = self.load_image_arrays(path_to_queries)
-        self.test_docs = self.load_image_arrays(path_to_test)
-
+        #print(path_to_queries, path_to_test)
+        self.query_docs = self.load_image_set(path_to_queries)
+        self.test_docs = self.load_image_set(path_to_test)
 
     @staticmethod
-    def load_image_arrays(path: str) -> dict: 
-        _docs = {doc: os.path.join(path, doc) for doc in os.listdir(path) if doc.endswith('.jpg')}
-        return {doc: array(Image.open(_docs[doc])) for doc in _docs} # Save each image as a Numpy Array
+    def load_image_set(path: str) -> dict: 
+        return {doc: os.path.join(path, doc) for doc in os.listdir(path) if doc.endswith('.jpg')}
 
-
+    
 class HashedDataset(Dataset):
     def __init__(self, hashing_function: FunctionType, *args, **kwargs) -> None:
         super(HashedDataset, self).__init__(*args, **kwargs)
         self.hasher = hashing_function
+        # self.test_hashes = {doc: str(self.hasher(Image.open(self.test_docs[doc]))) for doc in self.test_docs}
+        # self.query_hashes = {doc: str(self.hasher(Image.open(self.query_docs[doc]))) for doc in self.query_docs}
         self.fingerprint()
         self.doc2hash = deepcopy(self.test_hashes)
-        self.doc2hash.update(self.query_hashes)
+        #self.doc2hash.update(self.query_hashes)
         self.hash2doc = {self.doc2hash[doc]: doc for doc in self.doc2hash}
 
     
     def fingerprint(self) -> None:
-        self.test_hashes = {doc: str(self.hasher(self.test_docs[doc])) for doc in self.test_docs}
-        self.query_hashes = {doc: str(self.hasher(self.query_docs[doc])) for doc in self.query_docs}
+        self.test_hashes = {doc: str(self.hasher(Image.open(self.test_docs[doc]))) for doc in self.test_docs}
+        self.query_hashes = {doc: str(self.hasher(Image.open(self.query_docs[doc]))) for doc in self.query_docs}
+        
+        
+    def get_hashes(self) -> dict:
+        return self.doc2hash
+
+
+    def get_query_hashes(self) -> dict:
+        return self.query_hashes
+
+
+    def get_test_hashes(self) -> dict:
+        return self.test_hashes
--- imagededup/retrieval.py
+++ imagededup/retrieval.py
@@ -1,4 +1,5 @@
 import shelve
+import os
 
 class ResultSet:
     """In order to retrieve duplicate images an index needs to be built against which
@@ -7,14 +8,19 @@ class ResultSet:
 
     Takes input dictionary of image hashes for which DB has to be created."""
     def __init__(self, index_save_path: str, candidates:dict, queries: dict) -> None:
+        self.db_path = f'{index_save_path}.db'
         self.db = self.create_db_index(index_save_path)
         self.populate_db(candidates)
-        self.query_results = self.find_nearest_neighbors(queries)
-    
-    
+        self.fetch_nearest_neighbors(queries)
+        self.destroy_db_index()
+
     @staticmethod
     def create_db_index(path) -> shelve.DbfilenameShelf:
-        return shelve.open(path, writeback = True)
+        return shelve.open(path, writeback=True)
+
+
+    def refresh_db_buffer(self) -> shelve.DbfilenameShelf:
+        return shelve.open(self.db_path)
 
 
     def populate_db(self, candidates: dict):
@@ -24,9 +30,15 @@ class ResultSet:
         self.db.close()
 
 
-    def find_nearest_neighbors(self, queries):
-        return {query: self.db[queries[query]][:5] for query in queries}
+    def fetch_nearest_neighbors(self, queries) -> None:
+        self.db = self.refresh_db_buffer()
+        self.query_results = {query: self.db[queries[query]] for query in queries}
+        self.db.close()
 
-
+        
+    def destroy_db_index(self) -> None:
+        if self.query_results and os.path.exists(self.db_path):
+            os.remove(self.db_path)
+            
     def retrieve_results(self):
         return self.query_results
