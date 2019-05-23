59fcc401623584aba04d09b717b819f202c8eb67
valiantone <zrjohn@yahoo.com>
2019-05-23 20:35:44 +0200

    DS-529: Complete working example; needs unit tests
    Former-commit-id: d0b6d38ba85e15d7cc00ca6ba741f560774d97ce

--- imagededup/hashing.py
+++ imagededup/hashing.py
@@ -5,12 +5,8 @@ from PIL import Image
 from pathlib import Path
 from types import FunctionType
 from typing import Tuple
-
-"""
-TODO:
-refactor: Make another function for hash generation given 8 by 8 hash matrix
-Add wavelet hash?
-"""
+import random
+from copy import deepcopy
 
 
 class Hashing:
@@ -99,3 +95,48 @@ class Hashing:
 
     def dhash_dir(self, path_dir: Path) -> dict:
         return self.run_hash_on_dir(path_dir, self.dhash)
+
+
+class Dataset:
+    """
+    Class wrapper to instantiate a Dataset object composing of a subset of test images
+    and a smaller fraction of images that are used as queries to test search and retrieval.
+    Object contains hashed image fingerprints as well, however, hashing method is set by user.
+    """
+    def __init__(self, path_to_queries: str, path_to_test: str) -> None:
+        self.query_docs = self.load_image_set(path_to_queries)
+        self.test_docs = self.load_image_set(path_to_test)
+
+    @staticmethod
+    def load_image_set(path: str) -> dict: 
+        return {doc: os.path.join(path, doc) for doc in os.listdir(path) if doc.endswith('.jpg')}
+
+
+class HashedDataset(Dataset):
+    def __init__(self, hashing_function: FunctionType, *args, **kwargs) -> None:
+        super(HashedDataset, self).__init__(*args, **kwargs)
+        self.hasher = hashing_function
+        self.fingerprint()
+        self.doc2hash = deepcopy(self.test_hashes)
+        self.doc2hash.update(self.query_hashes)
+        self.hash2doc = {self.doc2hash[doc]: doc for doc in self.doc2hash}
+
+    
+    def fingerprint(self) -> None:
+        self.test_hashes = {doc: str(self.hasher(np.array(Image.open(self.test_docs[doc])))) for doc in self.test_docs}
+        self.query_hashes = {doc: str(self.hasher(np.array(Image.open(self.query_docs[doc])))) for doc in self.query_docs}
+        
+        
+    def get_docmap(self) -> dict:
+        return self.hash2doc
+    
+    def get_hashmap(self) -> dict:
+        return self.doc2hash
+
+
+    def get_query_hashes(self) -> dict:
+        return self.query_hashes
+
+
+    def get_test_hashes(self) -> dict:
+        return self.test_hashes
