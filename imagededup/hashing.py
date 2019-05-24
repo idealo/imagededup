Corrected hamming distance calculation.
Former-commit-id: 57f247bbd79519434ce5e76987c3b73339180589

imagededup/hashing.py
@@ -21,8 +21,9 @@ class Hashing:
 
     @staticmethod
     def hamming_distance(hash1: str, hash2: str) -> float:
-        hash1_bin = bin(int(hash1, 16))[2:]
-        hash2_bin = bin(int(hash2, 16))[2:]
+        hash1_bin = bin(int(hash1, 16))[2:].zfill(64)
+        hash2_bin = bin(int(hash2, 16))[2:].zfill(64)
+        assert len(hash1_bin) == len(hash2_bin)
         return np.sum([i != j for i, j in zip(hash1_bin, hash2_bin)])
 
     @staticmethod