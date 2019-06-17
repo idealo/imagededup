3bf788a5ccec3aa434542bd9da13b5127905eb28
tanujjain <tanuj.jain.10@gmail.com>
2019-06-17 14:24:41 +0200

    DS-523: Moved cosine evaluation to retrieval and added logging.

--- imagededup/cnn.py
+++ imagededup/cnn.py
@@ -1,3 +1,5 @@
+from imagededup.retrieval import CosEval
+from imagededup.logger import return_logger
 from keras.models import Model
 from keras.applications import MobileNet as ConvNet
 from keras.applications.mobilenet import preprocess_input
@@ -6,11 +8,9 @@ from keras.layers import Flatten
 from pathlib import Path, PosixPath
 from typing import Tuple
 from PIL import Image
-from numpy.linalg import norm
+import os
 import numpy as np
-# TODO: remove filenames from own retrievals
-# TODO: Move CosEval to retireval file
-# TODO: Add logging to denote the stage at which the current process has reached
+
 # TODO: Add a function for deleting detected duplicates
 # TODO: Write tests
 
@@ -18,12 +18,15 @@ import numpy as np
 class CNN:
     def __init__(self) -> None:
         self.model_full = ConvNet(include_top=True)
-        x = Flatten()(self.model_full.layers[-3].output) # hard-coded slice at -3 (conv layer closest to the output layer)
+        x = Flatten()(self.model_full.layers[-3].output)  # hard-coded slice at -3 (conv layer closest to output layer)
         self.model = Model(inputs=self.model_full.input, outputs=[x])
         self.TARGET_SIZE = (224, 224)
         self.BATCH_SIZE = 64
         self.file_mapping = None
         self.feat_vec = None
+        self.logger = return_logger(__name__, os.getcwd())
+        self.logger.info('Initialized: MobileNet pretrained on ImageNet dataset sliced at Convolutional layer closest'
+                         'to o/p layer')
 
     def _image_preprocess(self, pillow_image: Image) -> np.ndarray:
         im_res = pillow_image.resize(self.TARGET_SIZE)
@@ -35,7 +38,7 @@ class CNN:
             if isinstance(path_image, Path):
                 im = Image.open(path_image)
             elif isinstance(path_image, np.ndarray):
-                im = path_image.astype('uint8') # fromarray can't take float32/64
+                im = path_image.astype('uint8')  # fromarray can't take float32/64
                 im = Image.fromarray(im)
             else:
                 raise Exception
@@ -76,57 +79,22 @@ class CNN:
         return img_batches
 
     def cnn_dir(self, path_dir: Path) -> Tuple[np.ndarray, dict]:
+        self.logger.info('Start: Image feature generation')
         image_generator = self._generator(path_dir)
         self.feat_vec = self.model.predict_generator(image_generator, len(image_generator), verbose=1)
+        self.logger.info('Completed: Image feature generation')
         filenames = [i.split('/')[-1] for i in image_generator.filenames]
         dict_file_feature = {filenames[i]: self.feat_vec[i] for i in range(len(filenames))}
         self.file_mapping = dict(zip(range(len(image_generator.filenames)), filenames))
         return dict_file_feature
 
-    def find_duplicates(self, path_dir: PosixPath, threshold: float = 0.8):
+    def find_duplicates(self, path_dir: PosixPath, threshold: float = 0.8) -> dict:
         """Takes in path of the directory on which duplicates are to be detected.
         Returns dictionary containing key as filename and value as a list of duplicate filenames"""
         _ = self.cnn_dir(path_dir)
+        self.logger.info('Start: Evaluating similarity for getting duplicates')
         dict_ret = CosEval(self.feat_vec, self.feat_vec).get_retrievals_at_thresh(file_mapping_query=self.file_mapping,
                                                                                   file_mapping_ret=self.file_mapping,
                                                                                   thresh=threshold)
-        return dict_ret
-
-
-class CosEval:
-    def __init__(self, query_vector, ret_vector):
-        self.query_vector = query_vector
-        self.ret_vector = ret_vector
-        self.sim_mat = None
-        self.normalize_vector_matrices()
-
-    def normalize_vector_matrices(self):
-        self.normed_query_vector = self.get_normalized_matrix(self.query_vector)
-        self.normed_ret_vector = self.get_normalized_matrix(self.ret_vector)
-
-    @staticmethod
-    def get_normalized_matrix(x):
-        x_norm_per_row = norm(x, axis=1)
-        x_norm_per_row = x_norm_per_row[:, np.newaxis]  # adding another axis
-        x_norm_per_row_tiled = np.tile(x_norm_per_row, (1, x.shape[1]))
-        x_normalized = x / x_norm_per_row_tiled
-        return x_normalized
-
-    def _get_similarity(self):
-        self.sim_mat = np.dot(self.normed_query_vector, self.normed_ret_vector.T)
-
-    @staticmethod
-    def get_matches_above_threshold(row, thresh):
-        valid_inds = np.where(row >= thresh)[0]
-        valid_vals = row[valid_inds]
-        return valid_inds, valid_vals
-
-    def get_retrievals_at_thresh(self, file_mapping_query, file_mapping_ret, thresh=0.8):
-        dict_ret = {}
-        self._get_similarity()
-        for i in range(self.sim_mat.shape[0]):
-            valid_inds, valid_vals = self.get_matches_above_threshold(self.sim_mat[i, :], thresh)
-            retrieved_files = [file_mapping_ret[j] for j in valid_inds]
-            query_name = file_mapping_query[i]
-            dict_ret[query_name] = dict(zip(retrieved_files, valid_vals))
+        self.logger.info('End: Evaluating similarity for getting duplicates')
         return dict_ret
--- imagededup/retrieval.py
+++ imagededup/retrieval.py
@@ -1,5 +1,10 @@
 from imagededup.bktree import BKTree
+from imagededup.logger import return_logger
 from types import FunctionType
+from numpy.linalg import norm
+from typing import Tuple
+import os
+import numpy as np
 
 """
 TODO: Choose whether to run brute force or bktree search.
@@ -12,7 +17,8 @@ class ResultSet:
         self.queries = queries
         self.hamming_distance_invoker = hammer
         # self.fetch_nearest_neighbors_brute_force()
-        self.fetch_nearest_neighbors_bktree() # Keep bktree as the default search method instead of brute force
+        self.fetch_nearest_neighbors_bktree()  # Keep bktree as the default search method instead of brute force
+        # self.logger = return_logger(__name__, os.getcwd())
 
     def fetch_query_result_brute_force(self, query) -> dict:
         hammer = self.hamming_distance_invoker
@@ -20,15 +26,19 @@ class ResultSet:
         return {item: hammer(query, candidates[item]) for item in candidates if hammer(query, candidates[item]) < 5}
 
     def fetch_nearest_neighbors_brute_force(self) -> None:
+        # self.logger.info('Start: Retrieving duplicates using Brute force algorithm')  # TODO: Add max hamming distance
+        # after it is parmatrized
         sorted_results, sorted_distances = {}, {}
         for each in self.queries.values():
             res = self.fetch_query_result_brute_force(each)
             sorted_results[each] = sorted(res, key=lambda x: res[x], reverse=False)
             sorted_distances[each] = res.values() # REQUEST: Sort values too
-        self.query_results = sorted_results # REQUEST: Have key as filenames and not hashes
-        self.query_distances = sorted_distances # REQUEST: Change return types from dict_values to list, also have key as filenames and not hashes
+        self.query_results = sorted_results  # REQUEST: Have key as filenames and not hashes
+        self.query_distances = sorted_distances  # REQUEST: Change return types from dict_values to list, also have key as filenames and not hashes
 
     def fetch_nearest_neighbors_bktree(self) -> None:
+        # self.logger.info('Start: Retrieving duplicates using BKTree algorithm')  # TODO: Add max hamming distance after
+        # it is parmatrized
         dist_func = self.hamming_distance_invoker
         built_tree = BKTree(self.candidates, dist_func)  # construct bktree
 
@@ -45,3 +55,51 @@ class ResultSet:
     
     def retrieve_distances(self) -> dict:
         return self.query_distances
+
+
+class CosEval:
+    def __init__(self, query_vector: np.ndarray, ret_vector: np.ndarray) -> None:
+        self.query_vector = query_vector
+        self.ret_vector = ret_vector
+        self.logger = return_logger(__name__, os.getcwd())
+        self.normalize_vector_matrices()
+        self.sim_mat = None
+
+    def normalize_vector_matrices(self) -> None:
+        self.logger.info('Start: Vector normalization for computing cosine similarity')
+        self.normed_query_vector = self.get_normalized_matrix(self.query_vector)
+        self.normed_ret_vector = self.get_normalized_matrix(self.ret_vector)
+        self.logger.info('Completed: Vector normalization for computing cosine similarity')
+
+    @staticmethod
+    def get_normalized_matrix(x: np.ndarray) -> np.ndarray:
+        x_norm_per_row = norm(x, axis=1)
+        x_norm_per_row = x_norm_per_row[:, np.newaxis]  # adding another axis
+        x_norm_per_row_tiled = np.tile(x_norm_per_row, (1, x.shape[1]))
+        x_normalized = x / x_norm_per_row_tiled
+        return x_normalized
+
+    def _get_similarity(self) -> None:
+        self.logger.info('Start: Cosine similarity matrix computation')
+        self.sim_mat = np.dot(self.normed_query_vector, self.normed_ret_vector.T)
+        self.logger.info('End: Cosine similarity matrix computation')
+
+    def get_matches_above_threshold(self, row: int, thresh: float) -> Tuple[np.ndarray, np.ndarray]:
+        valid_inds = np.where(row >= thresh)[0]
+        valid_vals = row[valid_inds]
+        return valid_inds, valid_vals
+
+    def get_retrievals_at_thresh(self, file_mapping_query: dict, file_mapping_ret: dict, thresh=0.8) -> dict:
+        self.logger.info(f'Start: Getting duplicates with similarity above threshold = {thresh}')
+        dict_ret = {}
+        self._get_similarity()
+        for i in range(self.sim_mat.shape[0]):
+            valid_inds, valid_vals = self.get_matches_above_threshold(self.sim_mat[i, :], thresh)
+            retrieved_files = [file_mapping_ret[j] for j in valid_inds]
+            query_name = file_mapping_query[i]
+            if query_name in retrieved_files:
+                retrieved_files.remove(query_name)
+            dict_ret[query_name] = dict(zip(retrieved_files, valid_vals))
+        self.logger.info(f'End: Getting duplicates with similarity above threshold = {thresh}')
+        return dict_ret
+
