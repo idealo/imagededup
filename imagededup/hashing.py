11746ad1eed3a53d06c50001fc1db786a2548711
tanujjain <tanuj.jain.10@gmail.com>
2019-06-28 14:36:20 +0200

    Made image_preprocess and convert to image consistent across hash and cnn methods and added tests. Also corrected types in type annotations.

--- imagededup/hashing.py
+++ imagededup/hashing.py
@@ -3,9 +3,9 @@ import pywt
 import scipy.fftpack
 import numpy as np
 from PIL import Image
-from pathlib import Path
+from pathlib import Path, PosixPath
 from types import FunctionType
-from typing import Tuple
+from typing import Tuple, Dict
 from copy import deepcopy
 """
 TODO:
@@ -15,9 +15,10 @@ Wavelet hash: Allow possibility of different wavelet functions
 
 For all: Restrict image sizes to be greater than a certain size
 Allow acceptance of os.path in addition to already existing Path and numpy image array.
-Refactor: image_preprocess and convert_to_array as done in cnn.py
 
-Test: test convert_to_array wot input array having datatype other than unit8 (float32/64)
+Raise TypeError if an input other than Path or numpy array is provided as input to duplicates functions. (Check Input 
+Format! Input should be either a Path Variable or a numpy array!)
+
 """
 
 
@@ -38,7 +39,7 @@ class Hashing:
         return np.sum([i != j for i, j in zip(hash1_bin, hash2_bin)])
 
     @staticmethod
-    def run_hash_on_dir(path_dir: Path, hashing_function: FunctionType) -> dict:
+    def run_hash_on_dir(path_dir: Path, hashing_function: FunctionType) -> Dict:
         filenames = [os.path.join(path_dir, i) for i in os.listdir(path_dir) if i != '.DS_Store']
         hash_dict = dict(zip(filenames, [None] * len(filenames)))
         for i in filenames:
@@ -46,29 +47,34 @@ class Hashing:
         return hash_dict
 
     @staticmethod
-    def image_preprocess(path_image: Path, resize_dims: Tuple[int, int]) -> np.array:
-        im = Image.open(path_image)
-        im_res = im.resize(resize_dims, Image.ANTIALIAS)
-        im_gray = im_res.convert('L')  # convert to grayscale (i.e., single channel)
-        im_gray_arr = np.array(im_gray)
-        return im_gray_arr
+    def _image_preprocess(pillow_image: Image, resize_dims: Tuple[int, int] = (8, 8)) -> np.ndarray:
+        """
+        Resizes and typecasts a pillow image to numpy array.
 
-    def convert_to_array(self, path_image: None, resize_dims: Tuple[int, int] = (8, 8)) -> np.ndarray:
-        try:
-            if isinstance(path_image, Path):
-                im_gray_arr = self.image_preprocess(path_image, resize_dims)
-            elif isinstance(path_image, np.ndarray):
-                im = path_image.astype('uint8')  # fromarray can't take float32/64
-                im = Image.fromarray(im)
-                im_res = im.resize(resize_dims, Image.ANTIALIAS)
-                im_gray = im_res.convert('L')
-                im_gray_arr = np.array(im_gray)
-            else:
-                raise Exception
-            return im_gray_arr
-        except Exception:
-            print('Check Input Format! Input should be either a Path Variable or a numpy array!')
-            raise
+        :param pillow_image: A Pillow type image to be processed.
+        :return: A numpy array of processed image.
+        """
+
+        im_res = pillow_image.resize(resize_dims, Image.ANTIALIAS)
+        im_gray = im_res.convert('L')  # convert to grayscale (i.e., single channel)
+        im_arr = np.array(im_gray)
+        return im_arr
+
+    def _convert_to_array(self, path_image=None, resize_dims: Tuple[int, int] = (8, 8)) -> np.ndarray:
+        """
+        Accepts either path of an image or a numpy array and processes it to feed it to CNN.
+
+        :param path_image: PosixPath to the image file or Image typecast to numpy array.
+        :return: A processed image as numpy array
+        """
+
+        if isinstance(path_image, PosixPath):
+            im = Image.open(path_image)
+        elif isinstance(path_image, np.ndarray):
+            im = path_image.astype('uint8')  # fromarray can't take float32/64
+            im = Image.fromarray(im)
+        im_arr = self._image_preprocess(im, resize_dims)
+        return im_arr
 
     def get_hash(self, hash_mat: np.array, n_blocks: int) -> str:
         calculated_hash = []
@@ -79,7 +85,7 @@ class Hashing:
     def phash(self, path_image: None) -> str:
         """Implementation reference: http://www.hackerfactor.com/blog/index.php?/archives/432-Looks-Like-It.html"""
         res_dims = (32, 32)
-        im_gray_arr = self.convert_to_array(path_image, resize_dims=res_dims)
+        im_gray_arr = self._convert_to_array(path_image, resize_dims=res_dims)
         dct_coef = scipy.fftpack.dct(scipy.fftpack.dct(im_gray_arr, axis=0), axis=1)
         dct_reduced_coef = dct_coef[:8, :8]  # retain top left 8 by 8 dct coefficients
         mean_coef_val = np.mean(np.ndarray.flatten(dct_reduced_coef)[1:])  # average of coefficients excluding the DC
@@ -89,7 +95,7 @@ class Hashing:
 
     def ahash(self, path_image: Path) -> str:
         res_dims = (8, 8)
-        im_gray_arr = self.convert_to_array(path_image, resize_dims=res_dims)
+        im_gray_arr = self._convert_to_array(path_image, resize_dims=res_dims)
         avg_val = np.mean(im_gray_arr)
         hash_mat = im_gray_arr >= avg_val
         return self.get_hash(hash_mat, 16)  # 16 character output
@@ -97,14 +103,14 @@ class Hashing:
     def dhash(self, path_image: Path) -> str:
         """Implementation reference: http://www.hackerfactor.com/blog/index.php?/archives/529-Kind-of-Like-That.html"""
         res_dims = (9, 8)
-        im_gray_arr = self.convert_to_array(path_image, resize_dims=res_dims)
+        im_gray_arr = self._convert_to_array(path_image, resize_dims=res_dims)
         # hash_mat = im_gray_arr[:, :-1] > im_gray_arr[:, 1:]  # Calculates difference between consecutive columns
         hash_mat = im_gray_arr[:, 1:] > im_gray_arr[:, :-1]
         return self.get_hash(hash_mat, 16)  # 16 character output
 
     def whash(self, path_image: None) -> str:
         res_dims = (256, 256)
-        im_gray_arr = self.convert_to_array(path_image, resize_dims=res_dims)
+        im_gray_arr = self._convert_to_array(path_image, resize_dims=res_dims)
         coeffs = pywt.wavedec2(im_gray_arr, 'haar', level=5)  # decomposition level set to 5 to get 8 by 8 hash matrix
         LL_coeff = coeffs[0]
 
@@ -112,14 +118,17 @@ class Hashing:
         hash_mat = LL_coeff >= mean_coef_val  # All coefficients greater than mean of coefficients
         return self.get_hash(hash_mat, 16)  # 16 character output
 
-    def phash_dir(self, path_dir: Path) -> dict:
+    def phash_dir(self, path_dir: Path) -> Dict:
         return self.run_hash_on_dir(path_dir, self.phash)
 
-    def ahash_dir(self, path_dir: Path) -> dict:
+    def ahash_dir(self, path_dir: Path) -> Dict:
         return self.run_hash_on_dir(path_dir, self.ahash)
 
-    def dhash_dir(self, path_dir: Path) -> dict:
+    def dhash_dir(self, path_dir: Path) -> Dict:
         return self.run_hash_on_dir(path_dir, self.dhash)
+
+    def whash_dir(self, path_dir: Path) -> Dict:
+        return self.run_hash_on_dir(path_dir, self.whash)
 
 
 class Dataset:
@@ -133,7 +142,7 @@ class Dataset:
         self.test_docs = self.load_image_set(path_to_test)
 
     @staticmethod
-    def load_image_set(path: str) -> dict:
+    def load_image_set(path: str) -> Dict:
         return {doc: os.path.join(path, doc) for doc in os.listdir(path) if doc.endswith('.jpg')}
 
 
@@ -150,14 +159,14 @@ class HashedDataset(Dataset):
         self.test_hashes = {doc: str(self.hasher(np.array(Image.open(self.test_docs[doc])))) for doc in self.test_docs}
         self.query_hashes = {doc: str(self.hasher(np.array(Image.open(self.query_docs[doc])))) for doc in self.query_docs}
 
-    def get_docmap(self) -> dict:
+    def get_docmap(self) -> Dict:
         return self.hash2doc
 
-    def get_hashmap(self) -> dict:
+    def get_hashmap(self) -> Dict:
         return self.doc2hash
 
-    def get_query_hashes(self) -> dict:
+    def get_query_hashes(self) -> Dict:
         return self.query_hashes
 
-    def get_test_hashes(self) -> dict:
+    def get_test_hashes(self) -> Dict:
         return self.test_hashes
