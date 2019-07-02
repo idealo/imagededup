ca001739e21fd3ec2b1fe2b80094040f900a6493
tanujjain <tanuj.jain.10@gmail.com>
2019-07-02 18:32:59 +0200

    added convert ot array to image_utils.

--- imagededup/image_utils.py
+++ imagededup/image_utils.py
@@ -1,68 +1,101 @@
 import os
-from pathlib import PosixPath
-
+import numpy as np
+from PIL import Image
+from pathlib import Path, PosixPath
+from typing import Tuple
 
 """
-Validation of directory images
-For all: Restrict image sizes to be greater than a certain size
-Increase image input acceptance type for single images(jpg, bmp, png, jpeg, etc.)
 ? Allow acceptance of os.path in addition to already existing Path and numpy image array
-
+Todo:
+1. parallelize files validation
+2. add possibilities to ignore invalid directory images and still run hashes and cnn feat gen
 """
 
 
-def check_valid_file(path_image: PosixPath) -> int:
+def load_image(path: PosixPath) -> Image:
+    img = Image.open(path)
+
+    if img.mode != 'RGB':
+        # convert to RGBA first to avoid warning
+        # we ignore alpha channel if available
+        img = img.convert('RGBA').convert('RGB')
+    return img
+
+
+def load_valid_image(path_image: PosixPath, load=False):
+    """
+    Checks that file exists and is has a valid extension. If valid, loads and returns the image
+    :param path_image: PosixPath of the image
+    :param load: Flag to indicate if the valid image is to be loaded
+    :return: loaded image if the image is valid, else corresponding exception raised
+    """
     if not os.path.exists(path_image):
-        raise FileNotFoundError('Ensure that the file exists!')
+        raise FileNotFoundError('Ensure that the file exists at the specified path!')
     str_name = path_image.name
+
     if not (str_name.endswith('.jpeg') or str_name.endswith('.jpg') or str_name.endswith('.bmp') or
             str_name.endswith('.png')):
         raise TypeError('Image formats supported: .jpg, .jpeg, .bmp, .png')
-    return 1
-
-
-def check_directory_files(path_image: PosixPath):
-    """check if each image in a directory is a valid image
-    return 1 if all valid
-    return 0 if files can't be loaded. Also log info about missing files and continue with feature generation
-    if there is at least 1 valid file in the directory"""
-    return 0
+    if load:
+        try:
+            img = load_image(path_image)
+            return img
+        except Exception as e:
+            print(f'{e}: Image can not be loaded!')
+            raise e
+    else:
+        return 1
 
 
 def check_directory_files(path_dir: PosixPath):
-    """Checks if all files in *image_dir* are valid images.
-    Checks if files are images with extention 'JPEG' or 'PNG' and if they are not truncated.
-    self.logger.infos filenames that didn't pass the validation.
-    Sets:
-        self.valid_image_ids: a list of valid image.
+    """Checks if all files in path_dir are valid images.
     """
-    self.logger.info('\n****** Running image validation ******\n')
 
-    # validate images, use multiprocessing
-    files = [str(i.absolute()) for i in path_dir.glob('*')]
-    files.sort()
+    files = [Path(i.absolute()) for i in path_dir.glob('*') if not i.name.startswith('.')]  # ignore hidden files
 
-    results = parallelise(validate_image, files)
+    invalid_image_files = []
+    for i in files:
+        try:
+            load_valid_image(i, load=False)
+        except (FileNotFoundError, TypeError):
+            invalid_image_files.append(i)
+            
+    if len(invalid_image_files) != 0:
+        raise Exception(f'Please remove the following invalid files to run deduplication: {invalid_image_files}')
 
-    valid_image_files = [j for i, j in enumerate(files) if results[i][0]]
-    self.valid_image_ids = [Path(i).name for i in valid_image_files]
 
-    # return list of invalid images to user and save them if there are more than 10
-    invalid_image_files = [
-        (j, str(results[i][1])) for i, j in enumerate(files) if not results[i][0]
-    ]
+def _image_preprocess(pillow_image: Image, resize_dims: Tuple[int, int], hashmethod: bool = True) -> np.ndarray:
+    """
+    Resizes and typecasts a pillow image to numpy array.
 
-    if invalid_image_files:
-        self.logger.info('The following files are not valid image files:')
-        for file_name, error_msg in invalid_image_files[:10]:
-            self.logger.info('- {} ({})'.format(file_name, error_msg))
-        if len(invalid_image_files) > 10:
-            save_json(invalid_image_files, self.job_dir / 'invalid_image_files.json')
-            self.logger.info(
-                (
-                    'NOTE: More than 10 files were identified as invalid image files.\n'
-                    'The full list of those files has been saved here:\n{}'.format(
-                        self.job_dir / 'invalid_image_files.json'
-                    )
-                )
-            )
+    :param pillow_image: A Pillow type image to be processed.
+    :return: A numpy array of processed image.
+    """
+    if hashmethod:
+        im_res = pillow_image.resize(resize_dims, Image.ANTIALIAS)
+        im_res = im_res.convert('L')  # convert to grayscale (i.e., single channel)
+    else:
+        im_res = pillow_image.resize(resize_dims)
+
+    im_arr = np.array(im_res)
+    return im_arr
+
+
+def convert_to_array(path_image, resize_dims: Tuple[int, int], hashmethod: bool = True) -> np.ndarray:
+    """
+    Accepts either path of an image or a numpy array and processes it to feed it to CNN.
+
+    :param path_image: PosixPath to the image file or Image typecast to numpy array.
+    :return: A processed image as numpy array
+    """
+
+    if isinstance(path_image, PosixPath):
+        # im = Image.open(path_image)
+        im = load_valid_image(path_image=path_image, load=True)
+    elif isinstance(path_image, np.ndarray):
+        im = path_image.astype('uint8')  # fromarray can't take float32/64
+        im = Image.fromarray(im)
+    else:
+        raise TypeError('Check Input Format! Input should be either a Path Variable or a numpy array!')
+    im_arr = _image_preprocess(im, resize_dims, hashmethod)
+    return im_arr
