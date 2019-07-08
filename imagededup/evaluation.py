3a30778f5222b7ccbc88594fcf6abb2b165585f8
tanujjain <tanuj.jain.10@gmail.com>
2019-07-08 18:53:23 +0200

    Added missing docstrings to evaluation function.
    Former-commit-id: 9ac28924ff16a281706e0e81219aa533f6a08551

--- imagededup/evaluation.py
+++ imagededup/evaluation.py
@@ -5,15 +5,28 @@ from typing import List, Dict
 
 
 class EvalPerformance:
-    def __init__(self, dict_correct: Dict, dict_retrieved: Dict) -> None:
+    """
+    A class that provides possibilities to run evaluation for judging deduplication performance.
+    """
+    def __init__(self, dict_correct: Dict[str, List], dict_retrieved: Dict[str, List]) -> None:
+        """
+        Initialises dictionary of correct retrievals and retrieved obtained from deduplication algorithm.
+        :param dict_correct: dict of correct retrievals for each query(= ground truth),
+        # {'1.jpg': ['correct_dup1.jpg']}
+        :param dict_retrieved: dict of all retrievals for each query, {'1.jpg': ['retrieval_1.jpg']}
+        """
         self.dict_correct = dict_correct  # dict of correct retrievals for each query(= ground truth),
         # {'1.jpg': 'correct_dup1.jpg'}
         self.dict_retrieved = dict_retrieved  # dict of all retrievals for each query, {'1.jpg': 'retrieval_1.jpg'}
 
     @staticmethod
     def avg_prec(correct_duplicates: List, retrieved_duplicates: List) -> float:
-        """Input: (list of correct duplicates (i.e., ground truth), list of retrieved duplicates) for one single query
-        return: float representing average precision for one input query"""
+        """
+        Get average precision(AP) for a single query given correct and retrieved file names.
+        :param correct_duplicates: List of correct duplicates i.e., ground truth)
+        :param retrieved_duplicates: List of retrieved duplicates for one single query
+        :return: Average precision for this query.
+        """
         if not len(retrieved_duplicates):
             return 0.0
         count_real_correct = len(correct_duplicates)
@@ -26,8 +39,12 @@ class EvalPerformance:
 
     @staticmethod
     def ndcg(correct_duplicates: List, retrieved_duplicates: List) -> float:
-        """Input: (list of correct duplicates (i.e., ground truth), list of retrieved duplicates) for one single query
-                return: float representing Normalized discounted Cumulative Gain (NDCG) for one input query"""
+        """
+        Get Normalized discounted cumulative gain(NDCG) for a single query given correct and retrieved file names.
+        :param correct_duplicates: List of correct duplicates i.e., ground truth)
+        :param retrieved_duplicates: List of retrieved duplicates for one single query
+        :return: NDCG for this query.
+        """
         if not len(retrieved_duplicates):
             return 0.0
         relevance = np.array([1 if i in correct_duplicates else 0 for i in retrieved_duplicates])
@@ -55,8 +72,12 @@ class EvalPerformance:
 
     @staticmethod
     def jaccard_similarity(correct_duplicates: List, retrieved_duplicates: List) -> float:
-        """Input: (list of correct duplicates (i.e., ground truth), list of retrieved duplicates) for one single query
-                return: float representing jaccard similarity for one input query"""
+        """
+        Get jaccard similarity for a single query given correct and retrieved file names.
+        :param correct_duplicates: List of correct duplicates i.e., ground truth)
+        :param retrieved_duplicates: List of retrieved duplicates for one single query
+        :return: Jaccard similarity for this query.
+        """
         if not len(retrieved_duplicates):
             return 0.0
         set_correct_duplicates = set(correct_duplicates)
@@ -69,16 +90,23 @@ class EvalPerformance:
         return jacc_sim
 
     def mean_all_func(self, metric_func: FunctionType) -> float:
-        """Input: metric function on which mean is to be calculated across all queries
-                return: float representing mean of the metric across all queries"""
+        """
+
+        :param metric_func: metric function on which mean is to be calculated across all queries
+        :return: float representing mean of the metric across all queries
+        """
         all_metrics = []
         for k in self.dict_correct.keys():
             all_metrics.append(metric_func(self.dict_correct[k], self.dict_retrieved[k]))
         return np.mean(all_metrics)
 
     def get_all_metrics(self, save: bool = True) -> Dict:
-        """Input: Save flag indicating whether the dictionary below should be saved
-        return: dictionary of all mean metrics"""
+        """
+
+        :param save: Save flag indicating whether the dictionary below should be saved
+        :return: dictionary of all mean metrics
+        """
+
         dict_average_metrics = {
             'MAP': self.mean_all_func(self.avg_prec),
             'NDCG': self.mean_all_func(self.ndcg),
