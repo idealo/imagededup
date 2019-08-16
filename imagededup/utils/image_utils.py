5b83a45c717f1f5c6205716c7162a03d224e03e4
tanujjain <tanuj.jain.10@gmail.com>
2019-08-16 18:16:09 +0200

    Added pytest fixtures for common mocking.

--- imagededup/utils/image_utils.py
+++ imagededup/utils/image_utils.py
@@ -8,10 +8,7 @@ from typing import Tuple, List, Optional
 
 """
 ? Allow acceptance of os.path in addition to already existing Path and numpy image array
-Todo:
-1. parallelize files validation
-2. Add possibilities to ignore invalid directory images and still run hashes and cnn feat gen
-3. ? save invalid images to a file
+Todo: Parallelize files validation/ hash generation
 """
 
 
@@ -61,41 +58,40 @@ def load_image(image_file: Path, target_size=None, grayscale: bool = False,
         logger.warning(f'Invalid image file {image_file}:\n{e}')
         return None
 
-
-def _image_preprocess(pillow_image: Image, resize_dims: Tuple[int, int], for_hashing: bool = True) -> np.ndarray:
-    """
-    Resizes and typecasts a pillow image to numpy array.
-
-    :param pillow_image: A Pillow type image to be processed.
-    :return: A numpy array of processed image.
-    """
-    if for_hashing:
-        im_res = pillow_image.resize(resize_dims, Image.ANTIALIAS)
-        im_res = im_res.convert('L')  # convert to grayscale (i.e., single channel)
-    else:
-        im_res = pillow_image.resize(resize_dims)
-
-    im_arr = np.array(im_res)
-    return im_arr
-
-
-def convert_to_array(path_image, resize_dims: Tuple[int, int], for_hashing: bool = True) -> np.ndarray:
-    """
-    Accepts either path of an image or a numpy array and processes it to feed it to CNN or hashing methods.
-
-    :param path_image: PosixPath to the image file or Image typecast to numpy array.
-    :param resize_dims: Dimensions for resizing the image
-    :param for_hashing: Boolean flag to determine whether the function is being run for hashing or CNN based approach
-    :return: A processed image as numpy array
-    """
-
-    if isinstance(path_image, PosixPath):
-        # _validate_single_image(path_image)
-        im = load_image(image_file=path_image)
-    elif isinstance(path_image, np.ndarray):
-        im = path_image.astype('uint8')  # fromarray can't take float32/64
-        im = Image.fromarray(im)
-    else:
-        raise TypeError('Check Input Format! Input should be either a Path Variable or a numpy array!')
-    im_arr = _image_preprocess(im, resize_dims, for_hashing)
-    return im_arr
+# def _image_preprocess(pillow_image: Image, resize_dims: Tuple[int, int], for_hashing: bool = True) -> np.ndarray:
+#     """
+#     Resizes and typecasts a pillow image to numpy array.
+#
+#     :param pillow_image: A Pillow type image to be processed.
+#     :return: A numpy array of processed image.
+#     """
+#     if for_hashing:
+#         im_res = pillow_image.resize(resize_dims, Image.ANTIALIAS)
+#         im_res = im_res.convert('L')  # convert to grayscale (i.e., single channel)
+#     else:
+#         im_res = pillow_image.resize(resize_dims)
+#
+#     im_arr = np.array(im_res)
+#     return im_arr
+#
+#
+# def convert_to_array(path_image, resize_dims: Tuple[int, int], for_hashing: bool = True) -> np.ndarray:
+#     """
+#     Accepts either path of an image or a numpy array and processes it to feed it to CNN or hashing methods.
+#
+#     :param path_image: PosixPath to the image file or Image typecast to numpy array.
+#     :param resize_dims: Dimensions for resizing the image
+#     :param for_hashing: Boolean flag to determine whether the function is being run for hashing or CNN based approach
+#     :return: A processed image as numpy array
+#     """
+#
+#     if isinstance(path_image, PosixPath):
+#         # _validate_single_image(path_image)
+#         im = load_image(image_file=path_image)
+#     elif isinstance(path_image, np.ndarray):
+#         im = path_image.astype('uint8')  # fromarray can't take float32/64
+#         im = Image.fromarray(im)
+#     else:
+#         raise TypeError('Check Input Format! Input should be either a Path Variable or a numpy array!')
+#     im_arr = _image_preprocess(im, resize_dims, for_hashing)
+#     return im_arr
