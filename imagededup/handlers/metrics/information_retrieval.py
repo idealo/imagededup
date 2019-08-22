9da59ee4a35877b596bfb0cf2e5094581dffe9a8
tanujjain <tanuj.jain.10@gmail.com>
2019-08-22 18:48:54 +0200

    Fix path convention for test_information_retrieval.
    Former-commit-id: de536c77e52e56bf475f2da6764eb97008d3367b

--- imagededup/handlers/metrics/information_retrieval.py
+++ imagededup/handlers/metrics/information_retrieval.py
@@ -12,7 +12,9 @@ def avg_prec(correct_duplicates: List, retrieved_duplicates: List) -> float:
     if not len(retrieved_duplicates):
         return 0.0
     count_real_correct = len(correct_duplicates)
-    relevance = np.array([1 if i in correct_duplicates else 0 for i in retrieved_duplicates])
+    relevance = np.array(
+        [1 if i in correct_duplicates else 0 for i in retrieved_duplicates]
+    )
     relevance_cumsum = np.cumsum(relevance)
     prec_k = [relevance_cumsum[k] / (k + 1) for k in range(len(relevance))]
     prec_and_relevance = [relevance[k] * prec_k[k] for k in range(len(relevance))]
@@ -29,12 +31,17 @@ def ndcg(correct_duplicates: List, retrieved_duplicates: List) -> float:
     """
     if not len(retrieved_duplicates):
         return 0.0
-    relevance = np.array([1 if i in correct_duplicates else 0 for i in retrieved_duplicates])
+    relevance = np.array(
+        [1 if i in correct_duplicates else 0 for i in retrieved_duplicates]
+    )
     relevance_numerator = [2 ** (k) - 1 for k in relevance]
-    relevance_denominator = [np.log2(k + 2) for k in
-                             range(len(relevance))]  # first value of denominator term should be 2
+    relevance_denominator = [
+        np.log2(k + 2) for k in range(len(relevance))
+    ]  # first value of denominator term should be 2
 
-    dcg_terms = [relevance_numerator[k] / relevance_denominator[k] for k in range(len(relevance))]
+    dcg_terms = [
+        relevance_numerator[k] / relevance_denominator[k] for k in range(len(relevance))
+    ]
     dcg_k = np.sum(dcg_terms)
 
     # get #retrievals
@@ -45,11 +52,19 @@ def ndcg(correct_duplicates: List, retrieved_duplicates: List) -> float:
         ideal_dcg = np.sum([1 / np.log2(k + 2) for k in range(len(dcg_terms))])
         ndcg = dcg_k / ideal_dcg
     else:
-        ideal_dcg_terms = [1] * len(correct_duplicates) + [0] * (len(dcg_terms) - len(correct_duplicates))
-        ideal_dcg_numerator = [(2 ** ideal_dcg_terms[k]) - 1 for k in range(len(ideal_dcg_terms))]
+        ideal_dcg_terms = [1] * len(correct_duplicates) + [0] * (
+            len(dcg_terms) - len(correct_duplicates)
+        )
+        ideal_dcg_numerator = [
+            (2 ** ideal_dcg_terms[k]) - 1 for k in range(len(ideal_dcg_terms))
+        ]
         ideal_dcg_denominator = [np.log2(k + 2) for k in range(len(ideal_dcg_terms))]
-        ideal_dcg = np.sum([ideal_dcg_numerator[k] / ideal_dcg_denominator[k] for k in
-                            range(len(ideal_dcg_numerator))])
+        ideal_dcg = np.sum(
+            [
+                ideal_dcg_numerator[k] / ideal_dcg_denominator[k]
+                for k in range(len(ideal_dcg_numerator))
+            ]
+        )
         ndcg = dcg_k / ideal_dcg
     return ndcg
 
@@ -80,11 +95,7 @@ def mean_metric(ground_truth: Dict, retrieved: Dict, metric: str = None) -> floa
     :return: float representing mean of the metric across all queries
     """
     metric = metric.lower()
-    metric_lookup = {
-        'map': avg_prec,
-        'ndcg': ndcg,
-        'jaccard': jaccard_similarity
-    }
+    metric_lookup = {"map": avg_prec, "ndcg": ndcg, "jaccard": jaccard_similarity}
 
     metric_func = metric_lookup[metric]
     metric_vals = []
@@ -102,9 +113,8 @@ def get_all_metrics(ground_truth: Dict, retrieved: Dict) -> Dict:
     """
 
     all_average_metrics = {
-        'map': mean_metric(ground_truth, retrieved, metric='map'),
-        'ndcg': mean_metric(ground_truth, retrieved, metric='ndcg'),
-        'jaccard': mean_metric(ground_truth, retrieved, metric='jaccard'),
+        "map": mean_metric(ground_truth, retrieved, metric="map"),
+        "ndcg": mean_metric(ground_truth, retrieved, metric="ndcg"),
+        "jaccard": mean_metric(ground_truth, retrieved, metric="jaccard"),
     }
     return all_average_metrics
-
