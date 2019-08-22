0777d75be821646ac4fe0db5a7827e3bc32dfe77
tanujjain <tanuj.jain.10@gmail.com>
2019-08-22 14:44:56 +0200

    Changed paths of files to follow pathlib convention, made the tests concise.

--- imagededup/evaluation/evaluation.py
+++ imagededup/evaluation/evaluation.py
@@ -1,4 +1,7 @@
-from imagededup.handlers.metrics.information_retrieval import mean_metric, get_all_metrics
+from imagededup.handlers.metrics.information_retrieval import (
+    mean_metric,
+    get_all_metrics,
+)
 from typing import Dict
 
 
@@ -8,20 +11,20 @@ def _align_maps(ground_truth_map: Dict = None, duplicate_map: Dict = None):
 
     if not ground_truth_keys_set == duplicate_map_keys_set:
         diff = ground_truth_keys_set.difference(duplicate_map_keys_set)
-        assert diff == set(), f'Following keys not in duplicate_map:\n{diff}'
+        assert diff == set(), f"Following keys not in duplicate_map:\n{diff}"
     duplicate_map = {k: duplicate_map[k] for k in ground_truth_map.keys()}
     return duplicate_map
 
 
-def evaluate(ground_truth_map: Dict = None, duplicate_map: Dict = None, metric: str = 'all'):
+def evaluate(
+    ground_truth_map: Dict = None, duplicate_map: Dict = None, metric: str = "all"
+):
     metric = metric.lower()
     duplicate_map = _align_maps(ground_truth_map, duplicate_map)
 
-    if metric in ['map', 'ndcg', 'jaccard']:
+    if metric in ["map", "ndcg", "jaccard"]:
         return {metric: mean_metric(ground_truth_map, duplicate_map, metric=metric)}
-    elif metric == 'all':
+    elif metric == "all":
         return get_all_metrics(ground_truth_map, duplicate_map)
     else:
-        raise ValueError('Acceptable metrics are: \'map\', \'ndcg\', \'jaccard\', \'all\'')
-
-
+        raise ValueError("Acceptable metrics are: 'map', 'ndcg', 'jaccard', 'all'")
--- tests/test_evaluator.py
+++ tests/test_evaluator.py
@@ -1,19 +1,31 @@
 from imagededup.evaluation.evaluation import evaluate, _align_maps
-import os
 import pickle
 import pytest
+from pathlib import Path
+
+p = Path(__file__)
+
+PATH_GROUND_TRUTH = p.parent / "data/evaluation_files/ground_truth.pkl"
+PATH_ALL_CORRECT_RETRIEVALS = p.parent / "data/evaluation_files/all_correct_retrievals.pkl"
+PATH_INCORRECT_RETRIEVALS = p.parent / "data/evaluation_files/incorrect_retrievals.pkl"
+
 
 def load_pickle(filename):
-    """The path of the file below is set since the test suite is run using python -m pytest command from the image-dedup
-    directory"""
-    with open(os.path.join('tests', 'data', filename), 'rb') as f:
+    with open(filename, "rb") as f:
         dict_loaded = pickle.load(f)
     return dict_loaded
 
 
+def return_ground_all_correct_retrievals():
+    return load_pickle(PATH_GROUND_TRUTH), load_pickle(PATH_ALL_CORRECT_RETRIEVALS)
+
+
+def return_ground_incorrect_retrievals():
+    return load_pickle(PATH_GROUND_TRUTH), load_pickle(PATH_INCORRECT_RETRIEVALS)
+
+
 def test__align_maps_same_keys():
-    ground_truth_map = load_pickle('ground_truth.pkl')
-    retrieve_map = load_pickle('all_correct_retrievals.pkl')
+    ground_truth_map, retrieve_map = return_ground_all_correct_retrievals()
     duplicate_map = _align_maps(ground_truth_map, retrieve_map)
     assert set(duplicate_map.keys()) == set(ground_truth_map.keys())
     for i, j in zip(ground_truth_map.keys(), duplicate_map.keys()):
@@ -21,54 +33,58 @@ def test__align_maps_same_keys():
 
 
 def test__align_maps_different_keys():
-    ground_truth_map = load_pickle('ground_truth.pkl')
-    red_retrieve_map = {'ukbench09060.jpg': ground_truth_map['ukbench09060.jpg']}
+    ground_truth_map = load_pickle(PATH_GROUND_TRUTH)
+    red_retrieve_map = {"ukbench09060.jpg": ground_truth_map["ukbench09060.jpg"]}
     with pytest.raises(AssertionError):
         _align_maps(ground_truth_map, red_retrieve_map)
 
 
 def test_default_returns_all_metrics(mocker):
-    ground_truth_map = load_pickle('ground_truth.pkl')
-    retrieve_map = load_pickle('all_correct_retrievals.pkl')
-    get_all_metrics_mocker = mocker.patch('imagededup.evaluation.evaluation.get_all_metrics')
+    ground_truth_map, retrieve_map = return_ground_all_correct_retrievals()
+    get_all_metrics_mocker = mocker.patch(
+        "imagededup.evaluation.evaluation.get_all_metrics"
+    )
     evaluate(ground_truth_map, retrieve_map)
     get_all_metrics_mocker.assert_called_once_with(ground_truth_map, retrieve_map)
 
 
 def test_wrong_metric_raises_valueerror():
-    ground_truth_map = load_pickle('ground_truth.pkl')
-    retrieve_map = load_pickle('all_correct_retrievals.pkl')
+    ground_truth_map, retrieve_map = return_ground_all_correct_retrievals()
     with pytest.raises(ValueError):
-        evaluate(ground_truth_map, retrieve_map, metric='bla')
+        evaluate(ground_truth_map, retrieve_map, metric="bla")
 
 
-@pytest.mark.parametrize('metric_name', ['map', 'ndcg', 'jaccard'])
+@pytest.mark.parametrize("metric_name", ["map", "ndcg", "jaccard"])
 def test_correct_call_to_mean_metric(mocker, metric_name):
-    ground_truth_map = load_pickle('ground_truth.pkl')
-    retrieve_map = load_pickle('all_correct_retrievals.pkl')
-    mean_metric_mocker = mocker.patch('imagededup.evaluation.evaluation.mean_metric')
+    ground_truth_map, retrieve_map = return_ground_all_correct_retrievals()
+    mean_metric_mocker = mocker.patch("imagededup.evaluation.evaluation.mean_metric")
     evaluate(ground_truth_map, retrieve_map, metric=metric_name)
-    mean_metric_mocker.assert_called_once_with(ground_truth_map, retrieve_map, metric=metric_name)
+    mean_metric_mocker.assert_called_once_with(
+        ground_truth_map, retrieve_map, metric=metric_name
+    )
 
 
-@pytest.mark.parametrize('metric_name', ['MAP', 'Ndcg', 'JacCard'])
+@pytest.mark.parametrize("metric_name", ["MAP", "Ndcg", "JacCard"])
 def test_correct_call_to_mean_metric_mixed_cases(mocker, metric_name):
-    ground_truth_map = load_pickle('ground_truth.pkl')
-    retrieve_map = load_pickle('all_correct_retrievals.pkl')
-    mean_metric_mocker = mocker.patch('imagededup.evaluation.evaluation.mean_metric')
+    ground_truth_map, retrieve_map = return_ground_all_correct_retrievals()
+    mean_metric_mocker = mocker.patch("imagededup.evaluation.evaluation.mean_metric")
     evaluate(ground_truth_map, retrieve_map, metric=metric_name)
-    mean_metric_mocker.assert_called_once_with(ground_truth_map, retrieve_map, metric=metric_name.lower())
+    mean_metric_mocker.assert_called_once_with(
+        ground_truth_map, retrieve_map, metric=metric_name.lower()
+    )
+
 
 # Integration tests
 
 
-@pytest.mark.parametrize("metric, expected_value", [('map', 0.5555555555555556), ('ndcg', 0.6173196815056892),
-                                                        ('jaccard', 0.6)])
+@pytest.mark.parametrize(
+    "metric, expected_value",
+    [("map", 0.5555555555555556), ("ndcg", 0.6173196815056892), ("jaccard", 0.6)],
+)
 def test_correct_values(metric, expected_value):
     """Tests if correct MAP values are computed
     Load ground truth and dict for incorrect map prediction to have a Map less than 1.0"""
-    ground_truth = load_pickle('ground_truth.pkl')
-    retrieved = load_pickle('incorrect_retrievals.pkl')
+    ground_truth, retrieved = return_ground_incorrect_retrievals()
     score = evaluate(ground_truth, retrieved, metric=metric)
     assert isinstance(score, dict)
     assert list(score.keys())[0] == metric
@@ -76,10 +92,9 @@ def test_correct_values(metric, expected_value):
 
 
 def test_correct_values_all():
-    ground_truth = load_pickle('ground_truth.pkl')
-    retrieved = load_pickle('incorrect_retrievals.pkl')
+    ground_truth, retrieved = return_ground_incorrect_retrievals()
     score = evaluate(ground_truth, retrieved)
     assert isinstance(score, dict)
-    assert set(score.keys()) == set({'map', 'ndcg', 'jaccard'})
+    assert set(score.keys()) == set({"map", "ndcg", "jaccard"})
     for v in score.values():
         assert v is not None
