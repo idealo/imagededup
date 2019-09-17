75bc867d50cb42bc86c435b1032ce5d488c16872
tanujjain <tanuj.jain.10@gmail.com>
2019-09-17 16:15:53 +0200

    Resolve conflict aas part of rebasing.
    Former-commit-id: 7bfd8b638d7976327a3baf3bf5048846b46482ad

--- imagededup/utils/general_utils.py
+++ imagededup/utils/general_utils.py
@@ -1,5 +1,6 @@
 import os
 
+
 import json
 from typing import Dict, List
 
@@ -49,12 +50,3 @@ def save_json(results: Dict, filename: str) -> None:
         json.dump(results, f, indent=2, sort_keys=True)
     logger.info('End: Saving duplicates as json!')
 
-
-def parallelise(function, data):
-    num_process = multiprocessing.cpu_count()
-    chunk_len = len(data) // num_process
-    pool = multiprocessing.Pool(processes=num_process)
-    results = list(tqdm.tqdm(pool.imap(function, data), total=len(data)))
-    pool.close()
-    pool.join()
-    return results
