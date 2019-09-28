f79cbf61ab8aa4e6a5b9beaee91f49eb35f5a092
Dat Tran <datitran@gmail.com>
2019-09-28 19:04:03 +0200

    Remove unnecessary docs folder as some of them are auto-generated, modify setup.py and also add travis.
    Former-commit-id: 4da23ee3d7ecda2a66c5b7cb143b6d05d220682f

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='',
     packages=['imagededup'],
     url='',
-    license='MIT',
+    license='Apache 2.0',
     author='Tanuj Jain, Christopher Lennan, Zubin John',
     author_email='tanuj.jain.10@gmail.com, christopherlennan@gmail.com, zrjohn@yahoo.com',
     description='Package for image deduplication',
@@ -24,4 +24,16 @@ setup(
         'tests': ['pytest==4.4.1', 'pytest-cov==2.6.1', 'pytest-mock==1.10.4'],
         'docs': ['mkdocs==1.0.4', 'mkdocs-material==4.0.2'],
     },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    packages=find_packages(exclude=('tests',))
 )
