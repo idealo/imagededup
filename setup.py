82dbec739341cb2e83cff90b05bed5161c4b90ee
tanujjain <tanuj.jain.10@gmail.com>
2019-09-30 17:20:32 +0200

    Bump version to 0.0.3
    Former-commit-id: 50300a85e90122594a219e7b138477e6407d552b

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ functionality provided by the package:
 
 setup(
     name='imagededup',
-    version='0.0.2',
+    version='0.0.3',
     packages=find_packages(exclude=('tests',)),
     url='',
     long_description=long_description,
