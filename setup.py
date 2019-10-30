2fc1ad53d275fe84c9b0836c93ca2a3b52da8196
Dat Tran <datitran@gmail.com>
2019-10-30 14:48:11 +0100

    Bump up version to 0.2.0.

--- setup.py
+++ setup.py
@@ -89,7 +89,7 @@ else:
 
 setup(
     name='imagededup',
-    version='0.1.0',
+    version='0.2.0',
     author='Tanuj Jain, Christopher Lennan, Zubin John, Dat Tran',
     author_email='tanuj.jain.10@gmail.com, christopherlennan@gmail.com, zrjohn@yahoo.com, datitran@gmail.com',
     description='Package for image deduplication',
