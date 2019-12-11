6f341697819bc53c1cfc4c9ac0c7fab0696f438f
Tanuj Jain <tanuj.jain.10@gmail.com>
2019-12-11 10:48:58 +0100

    Bump version to 0.2.2.

--- setup.py
+++ setup.py
@@ -89,7 +89,7 @@ else:
 
 setup(
     name='imagededup',
-    version='0.2.1',
+    version='0.2.2',
     author='Tanuj Jain, Christopher Lennan, Zubin John, Dat Tran',
     author_email='tanuj.jain.10@gmail.com, christopherlennan@gmail.com, zrjohn@yahoo.com, datitran@gmail.com',
     description='Package for image deduplication',
