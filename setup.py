bcadb0060cacd734706075b419dbe10a0e9d4c62
Mohamed Rashad <mohamed_rashad@ieee.org>
2022-08-18 06:27:05 +0200

    fixing some reqs

--- setup.py
+++ setup.py
@@ -96,8 +96,8 @@ setup(
     long_description=long_description,
     license='Apache 2.0',
     install_requires=[
-        'tensorflow>1.0',
-        'Pillow<7.0.0',
+        'tensorflow',
+        'Pillow',
         'tqdm',
         'scikit-learn',
         'PyWavelets~=1.1.1',
