0d9342c01c924e6eb5b289328397b7e4c22e4ee7
Tanuj Jain <tanuj.jain.10@gmail.com>
2022-11-15 16:59:10 +0100

    Debugging broken documentation building by logging autogen.py.

--- mkdocs/autogen.py
+++ mkdocs/autogen.py
@@ -255,6 +255,7 @@ def extract_comments(directory):
         for file_name in file_names:
             if os.path.splitext(file_name)[1] == '.py' and file_name != '__init__.py':
                 # with open
+                print(f'Processing {file_name=}')
                 doc = get_comments_str(os.path.join(parent, file_name))
                 directory = os.path.join('docs', parent.replace('../imagededup/', ''))
                 if not os.path.exists(directory):
