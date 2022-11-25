d43777785566ab4fd2b055e8b63c5f321229653c
Prateek Ralhan <ralhanprateek@gmail.com>
2022-11-25 14:43:44 +0530

    Delete streamlit_webapp directory

--- streamlit_webapp/Procfile
+++
@@ -1,1 +0,0 @@
-web: sh setup.sh && streamlit run app.py
--- streamlit_webapp/setup.sh
+++
@@ -1,9 +0,0 @@
-mkdir -p ~/.streamlit/
-echo "[general]
-email = \"ralhanprateek@gmail.com\"
-" > ~/.streamlit/credentials.toml
-echo "[server]
-headless = true
-port = $PORT
-enableCORS = false
-" > ~/.streamlit/config.toml
