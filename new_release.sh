57a7e855eb00cbe824b63c265bc2ecc4dbee89dd
Tanuj Jain <tanuj.jain.10@gmail.com>
2025-06-26 17:26:51 +0200

    Updated release script to include twine, updated python version in pyproject toml.

--- new_release.sh
+++ new_release.sh
@@ -1,4 +1,7 @@
+# Usage: ./new_release.sh <version>
+# Example: ./new_release.sh post for post release, patch for patch release, minor for minor release, major for major release
 uv tool install bump-my-version
+uv pip install twine
 bump-my-version bump $1
 git add .
 git commit -m "Bump version to $(bump-my-version show current_version)"
@@ -8,6 +11,5 @@ uv build --sdist
 
 # Wait for wheels to be built via GA
 # Move downloaded wheels to dist/
-# Test wheels locally, if successful:
-# uv publish --path dist/
+# Test wheels locally, if successful: twine upload dist/*
 
